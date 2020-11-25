b16d30aa60ddd90853b3e45c26c7f404aeea0cb3
shubham <shubham@pop-os.localdomain>
2020-11-25 08:43:25 +0530

    abc

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ HOMEPAGE='https://www.wikipedia.org/'
 DOMAIN_NAME=get_domain_name(HOMEPAGE)
 QUEUE_FILE=PROJECT_NAME+'/queue.txt'
 CRAWLED_FILE=PROJECT_NAME+'/crawled.txt'
-NUMBER_OF_THREADS=4
+NUMBER_OF_THREADS=8
 queue=Queue() #thread queue
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
 
